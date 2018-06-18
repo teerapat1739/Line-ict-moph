adc886b75e4035207b2638bf2baa4c9f39e7af73
Teerapat Tee <teerapat12345678@gmail.com>
2018-06-19 03:35:12 +0700

    add drug

--- webhook/index.js
+++ webhook/index.js
@@ -131,7 +131,7 @@ async function handleMessageEvent(event) {
                 }]
             }
         }
-    } else if (eventText === 'food') {
+    } else if (eventText === 'drug') {
         msg = {
             "type": "template",
             "altText": "this is a carousel template",
@@ -139,13 +139,13 @@ async function handleMessageEvent(event) {
                 "type": "carousel",
                 "columns": [
                     {
-                        "thumbnailImageUrl": "https://github.com/teerapat1739/FoodSys/blob/master/assets/images/Cock.jpg?raw=true",
-                        "title": "จระเข้แดดเดียว",
-                        "text": "120 บาท",
+                        "thumbnailImageUrl": "https://teerapat-reminder.herokuapp.com/drug/benzac.jpg",
+                        "title": "Benzac",
+                        "text": "ทาหลังอาบน้า  10 - 15 นาที แล้วล้างออก",
                         "actions": [
                             {
                                 "type": "postback",
-                                "label": "Buy",
+                                "label": "เช้า-เย็น",
                                 "data": "action=buy&itemid=111"
                             },
                             {
@@ -161,13 +161,13 @@ async function handleMessageEvent(event) {
                         ]
                     },
                     {
-                        "thumbnailImageUrl": "https://github.com/jirawat050/exSF312/blob/master/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3/%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%94%E0%B8%B9%E0%B8%81%E0%B8%AD%E0%B9%88%E0%B8%AD%E0%B8%99%E0%B8%88%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%82%E0%B9%89.jpg?raw=true",
-                        "title": "กระดูกอ่อนจระเข้",
-                        "text": "100 บาท",
+                        "thumbnailImageUrl": "https://teerapat-reminder.herokuapp.com/drug/Valtrex.jpg",
+                        "title": "Valtrex",
+                        "text": "ทาหลังอาบน้า  บริเวณที่เป็นเริม",
                         "actions": [
                             {
                                 "type": "postback",
-                                "label": "Buy",
+                                "label": "เช้า-เย็น",
                                 "data": "action=buy&itemid=111"
                             },
                             {
@@ -183,13 +183,13 @@ async function handleMessageEvent(event) {
                         ]
                     },
                     {
-                        "thumbnailImageUrl": "https://github.com/jirawat050/exSF312/blob/master/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3/beef_green_curry_pea_eggplants.jpg?raw=true",
-                        "title": "แกงกะหรี่",
-                        "text": "100 บาท",
+                        "thumbnailImageUrl": "https://teerapat-reminder.herokuapp.com/drug/CalamineLotion.jpg",
+                        "title": "CalamineLotion",
+                        "text": "ทาหลังอาบน้า  บริเวณที่คัน",
                         "actions": [
                             {
                                 "type": "postback",
-                                "label": "Buy",
+                                "label": "เช้า-เย็น",
                                 "data": "action=buy&itemid=111"
                             },
                             {
@@ -204,28 +204,6 @@ async function handleMessageEvent(event) {
                             }
                         ]
                     },
-                    {
-                        "thumbnailImageUrl": "https://github.com/jirawat050/exSF312/blob/master/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3/%E0%B8%9C%E0%B8%B1%E0%B8%94%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%9E%E0%B8%A3%E0%B8%B2%E0%B8%AB%E0%B8%A1%E0%B8%B9_Stir-fried%20pork%20with%20holy%20basil%20leaves.jpg?raw=true",
-                        "title": "กระเพราหมู",
-                        "text": "50 บาท",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "Buy",
-                                "data": "action=buy&itemid=111"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Add to cart",
-                                "data": "action=add&itemid=111"
-                            },
-                            {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "https://front-sf312.azurewebsites.net"
-                            }
-                        ]
-                    }
                 ]
             }
         }
